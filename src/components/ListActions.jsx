f33f853a4a9c70022a5ea33ef0523a5bd1904a5e
Husam7664 <husam7664@gmail.com>
2022-04-29 13:29:37 +0530

    update

--- src/components/ListActions.jsx
+++ src/components/ListActions.jsx
@@ -10,27 +10,30 @@ function Action({ action }) {
           </div>
 
           <div className='bg-slate-100 rounded '>
-            {action.reverse().map((actions, key) => (
-              <div key={key}>
-                <ul className='list-none  '>
-                  <li className='border-solid  p-1 flex flex-col-reverse '>
-                    <div className=' '>
-                      <div className='p-3 pr-4 grid grid-cols-4 bg-white rounded'>
-                        <div className='col-span-3 text-gray-600 pt-2 pl-5 '>
-                          Moved post {actions.value - 1} from index{' '}
-                          {actions.value} to {actions.value - 1} index
-                        </div>
-                        <div className='col-span-1  '>
-                          <button className='bg-green-500 hover:bg-green-600 w-35 h-10 text-white font-bold rounded p-2 '>
-                            Time Travel
-                          </button>
+            {action
+              .slice()
+              .reverse()
+              .map((actions, key) => (
+                <div key={key}>
+                  <ul className='list-none  '>
+                    <li className='border-solid  p-1 flex flex-col-reverse '>
+                      <div className=' '>
+                        <div className='p-3 pr-4 grid grid-cols-4 bg-white rounded'>
+                          <div className='col-span-3 text-gray-600 pt-2 pl-5 '>
+                            Moved post {actions.value - 1} from index{' '}
+                            {actions.value} to {actions.value - 1} index
+                          </div>
+                          <div className='col-span-1  '>
+                            <button className='bg-green-500 hover:bg-green-600 w-35 h-10 text-white font-bold rounded p-2 '>
+                              Time Travel
+                            </button>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            ))}
+                    </li>
+                  </ul>
+                </div>
+              ))}
           </div>
         </div>
       </div>
